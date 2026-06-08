092eb0214cdc05a8a26a85feda5b313cdaf940c8
adkaralmusliam <othman19289@gmail.com>
2026-06-08 06:41:26 +0300

    build: root build.gradle.kts configuration

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("com.android.library") version "8.1.4" apply false
+    kotlin("android") version "1.9.10" apply false
+    kotlin("jvm") version "1.9.10" apply false
+    kotlin("plugin.serialization") version "1.9.10" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+    }
+}
